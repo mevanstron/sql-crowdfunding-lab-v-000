45006ed76ee1daec059c7dacd5cd1540eea7ce0b
Michael Evans <michael@michaelgregoryevans.com>
2017-11-06 17:40:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  "SELECT projects.title, SUM(pledges.amount) 
+  "SELECT projects.title, SUM(pledges.amount)
    FROM projects
    INNER JOIN pledges
    ON projects.id = pledges.project_id
@@ -16,7 +16,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, SUM(pledges.amount) 
+  "SELECT users.name, users.age, SUM(pledges.amount)
    FROM users
    INNER JOIN pledges
    ON users.id = pledges.user_id
@@ -25,7 +25,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS amount_over_goal 
+  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS amount_over_goal
    FROM projects
    INNER JOIN pledges
    ON projects.id = pledges.project_id
@@ -34,12 +34,12 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT users.name, SUM(pledges.amount) 
+  "SELECT users.name, SUM(pledges.amount)
    FROM users
    INNER JOIN pledges
    ON users.id = pledges.user_id
    GROUP BY users.name
-   
+
    "
 end
 
